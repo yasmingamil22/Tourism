1b2719701d7645668eabd80bedadf1588a4bcabd
yasmingamil22 <yasmingamy@gmail.com>
2020-04-13 20:29:39 +0200

    add countries sp

--- app/src/main/java/com/example/tourism/Main2Activity.java
+++ app/src/main/java/com/example/tourism/Main2Activity.java
@@ -2,13 +2,24 @@ package com.example.tourism;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
-
+Button bt_countries;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        bt_countries=findViewById(R.id.bt_countries);
+        bt_countries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Main2Activity.this,Countries.class);
+           startActivity(i);
+            }
+        });
     }
 }
