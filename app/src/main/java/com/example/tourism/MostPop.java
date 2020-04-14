3888e63355f07758dae17da741a822aa3da52670
شَدو محمد <60104410+shadodow@users.noreply.github.com>
2020-04-14 19:15:37 +0200

    navigation

--- app/src/main/java/com/example/tourism/MostPop.java
+++ app/src/main/java/com/example/tourism/MostPop.java
@@ -1,31 +1,27 @@
 package com.example.tourism;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-public class Cairo extends AppCompatActivity {
-RecyclerView Rv;
+public class MostPop extends AppCompatActivity {
+RecyclerView rec;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cairo);
-        Rv = findViewById(R.id.rv);
+        setContentView(R.layout.activity_most_pop);
+        rec = findViewById(R.id.rec);
         ArrayList<Place> places = new ArrayList<>();
         places.add(new Place(R.drawable.pyr, "pyramid"));
-        places.add(new Place(R.drawable.aesh, "aesha fahmy"));
-        places.add(new Place(R.drawable.cas, "castle"));
         Cov_adapter  adapter = new Cov_adapter(places);
         RecyclerView.LayoutManager lm= new LinearLayoutManager(this);
-        Rv.setHasFixedSize(true);
-        Rv.setLayoutManager(lm);
-        Rv.setAdapter(adapter);
+        rec.setHasFixedSize(true);
+        rec.setLayoutManager(lm);
+        rec.setAdapter(adapter);
 
-
+    }
 }
